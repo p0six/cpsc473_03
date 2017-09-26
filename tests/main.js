233ad0f967718d0a31517fbb5ab4ec5e57cc9455
Michael Romero <romerom@gmail.com>
2017-09-25 20:30:56 -0700

    updated test main

--- tests/main.js
+++ tests/main.js
@@ -1,13 +1,11 @@
-eval(require('fs').readFileSync('tests/datastore.js', 'utf8'));
-eval(require('fs').readFileSync('tests/truck.js', 'utf8'));
-
-(function(window) {
-  'use strict';
-  var App = window.App;
-  var Truck = App.Truck;
-  var DataStore = App.DataStore;
-  var myTruck = new Truck('ncc-1701', new DataStore());
-  window.myTruck = myTruck;
-  eval(require('fs').readFileSync('tests/test_datastore.js', 'utf8'));
-  eval(require('fs').readFileSync('tests/test_truck.js', 'utf8'));
-})(process);
+'use strict';
+var window = {};
+eval(require('fs').readFileSync('scripts/datastore.js', 'utf8'));
+eval(require('fs').readFileSync('scripts/truck.js', 'utf8'));
+var App = window.App;
+var Truck = App.Truck;
+var DataStore = App.DataStore;
+var myTruck = new Truck('ncc-1701', new DataStore());
+window.myTruck = myTruck;
+eval(require('fs').readFileSync('tests/test_datastore.js', 'utf8'));
+eval(require('fs').readFileSync('tests/test_truck.js', 'utf8'));
